1c8e8ba67f745b619dc9448ebadde4eb09fea182
Dionne <dionne.r.9@gmail.com>
2022-12-14 13:44:42 -0500

    healthkit init

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
+  appId: 'io.ionic.starter.project-leo-v2',
   appName: 'project-leo-v2',
   webDir: 'dist',
   bundledWebRuntime: false
