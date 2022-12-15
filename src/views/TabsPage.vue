c31e8b2a042d38b4211b580d474efc03b6447dd1
Dionne <dionne.r.9@gmail.com>
2022-12-15 17:25:54 -0500

    finish home page and notification component

--- src/views/TabsPage.vue
+++ src/views/TabsPage.vue
@@ -3,19 +3,24 @@
     <ion-tabs>
       <ion-router-outlet></ion-router-outlet>
       <ion-tab-bar slot="bottom">
-        <ion-tab-button tab="tab1" href="/tabs/tab1">
-          <ion-icon :icon="triangle" />
-          <ion-label>Tab 1</ion-label>
+        <ion-tab-button tab="Home" href="/tabs/tab1">
+          <img src="../assets/giving-LG.svg" />
+          <ion-label>Home</ion-label>
         </ion-tab-button>
           
-        <ion-tab-button tab="tab2" href="/tabs/tab2">
-          <ion-icon :icon="ellipse" />
-          <ion-label>Tab 2</ion-label>
+        <ion-tab-button tab="Rewards" href="/tabs/tab2">
+          <img src="../assets/medal-LG.png" />
+          <ion-label>Rewards</ion-label>
         </ion-tab-button>
         
-        <ion-tab-button tab="tab3" href="/tabs/tab3">
-          <ion-icon :icon="square" />
-          <ion-label>Tab 3</ion-label>
+        <ion-tab-button tab="Goals" href="/tabs/tab3">
+          <img src="../assets/archery-target-LG.png" />
+          <ion-label>Goals</ion-label>
+        </ion-tab-button>
+
+          <ion-tab-button tab="Doctors" href="/tabs/tab3">
+          <img src="../assets/doctor-LG.png" />
+          <ion-label>My Doctors</ion-label>
         </ion-tab-button>
       </ion-tab-bar>
     </ion-tabs>
@@ -24,12 +29,12 @@
 
 <script lang="ts">
 import { defineComponent } from 'vue';
-import { IonTabBar, IonTabButton, IonTabs, IonLabel, IonIcon, IonPage, IonRouterOutlet } from '@ionic/vue';
+import { IonTabBar, IonTabButton, IonTabs, IonLabel, IonPage, IonRouterOutlet } from '@ionic/vue';
 import { ellipse, square, triangle } from 'ionicons/icons';
 
 export default defineComponent({
   name: 'TabsPage',
-  components: { IonLabel, IonTabs, IonTabBar, IonTabButton, IonIcon, IonPage, IonRouterOutlet },
+  components: { IonLabel, IonTabs, IonTabBar, IonTabButton, IonPage, IonRouterOutlet },
   setup() {
     return {
       ellipse, 
